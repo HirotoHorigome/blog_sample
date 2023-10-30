55d409ad4c42f3a65a3df468ca81b3fcc96595f0
Hiroto Horigome <hiroto.horigome0528cc@gmail.com>
2023-10-30 11:50:15 +0900

    ルーティング完了

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// ブログ一覧画面を表示
+Route::get('/', 'BlogController@showList')->name('blogs');
